df27322d5842b8f44125cc0770015d76d83b32ed
FurkanAkkaya0 <furkanakkaya0@outlook.com>
2024-06-27 17:35:14 +0300

    ilk react proje kodları

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/logo192.png" alt=""/>
+    </div>
+  );
+}
+
+export default App;
